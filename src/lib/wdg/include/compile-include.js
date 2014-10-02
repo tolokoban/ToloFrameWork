c1959957d78c662a5f4170363caae306dd735512
Tolokoban <contact@tolokoban.org>
2014-10-02 17:51:09 +0200

    Better exception when file not found for <w:incllude>.

--- src/lib/wdg/include/compile-include.js
+++ src/lib/wdg/include/compile-include.js
@@ -8,13 +8,14 @@ var FS = require("fs");
 /**
  * @summary
  * Include a piece of HTML at current position.
+ * <w:include>page-help.htm</w:include>
  */
 module.exports.precompile = function(root) {
     var Tree = this.Tree;
     var filename = Tree.text(root).trim();
-    var file = this.srcPath(filename);
+    var file = this.srcOrLibPath(filename);
     if (!FS.existsSync(file)) {
-        throw "Include file not found: \"" + file + "\"!";
+        this.prj.fatal("Include file not found: \"" + file + "\"!", -1, "<w:include>");
     }
     var stat = FS.statSync(file);
     if (!stat.isFile) {
