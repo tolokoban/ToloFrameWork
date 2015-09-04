6e94e40f3e990acbd66af8bb04a59015572ddf3b
Tolokoban <contact@tolokoban.org>
2015-09-04 08:37:09 +0200

    Starting development of components.

---
+++ ker/com/default/default.com.js
@@ -0,0 +1,11 @@
+/**
+ * Default component.
+ */
+
+exports.tags = "*";
+exports.priority = -9999;
+
+exports.init = function(libs) {};
+exports.open = function(file, libs) {};
+exports.close = function(file, libs) {};
+exports.compile = function(root, libs) {};
