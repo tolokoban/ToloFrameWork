c20ce2c8dea52d42fa6dd96601ef077d42023644
Tolokoban <fabien.petitjean@free.fr>
2015-11-23 20:27:54 +0100

    New options {id} for Widget.

--- ker/mod/tfw.timer.js
+++ ker/mod/tfw.timer.js
@@ -14,7 +14,7 @@ exports.later = function(delay) {
  * @param action Promise to start after delay.
  * @param delay Milliseconds.
  */
-var Action = function(action, delay) {
+var ActionPromise = function(action, delay) {
     if (typeof delay !== 'number') delay = 300;
     if (delay < 0) delay = 0;
     var that = this;
@@ -28,7 +28,14 @@ var Action = function(action, delay) {
 /**
  * @return void
  */
-Action.prototype.fire = function() {
+ActionPromise.prototype.dbg = function(msg) {
+    console.log((this.enabled ? 'E' : 'e') + (this.waiting ? 'W' : 'w') + ": " + msg);
+};
+
+/**
+ * @return void
+ */
+ActionPromise.prototype.fire = function() {
     var that = this;
     if (this.timer) {
         window.clearTimeout(this.timer);
@@ -45,7 +52,7 @@ Action.prototype.fire = function() {
             function() {
                 that.timer = 0;
                 that.enabled = false;
-                that.action.then(f, f);
+                that.action().then(f, f);
             },
             that.delay
         );
@@ -55,6 +62,82 @@ Action.prototype.fire = function() {
 };
 
 
+/**
+ * @param action Function to start after delay.
+ * @param delay Milliseconds.
+ */
+var Action = function(action, delay) {
+    if (typeof delay !== 'number') delay = 300;
+    if (delay < 0) delay = 100;
+    var that = this;
+    this.action = action;
+    this.delay = delay;
+    this.timer = 0;
+};
+
+/**
+ * @return void
+ */
+Action.prototype.fire = function() {
+    var that = this;
+    if (this.timer) {
+        window.clearTimeout(this.timer);
+    }
+    this.timer = window.setTimeout(
+        function() {
+            that.timer = 0;
+            that.enabled = false;
+            that.action();
+        },
+        that.delay
+    );
+};
+
+var LongAction = function() {
+  this._timer = null;
+  this._action = null;
+};
+
+/**
+ * Fire an action. This action will be executed only if there is nothing else running.
+ * @return void
+ */
+LongAction.prototype.fire = function(action, duration) {
+  var that = this;
+  if (!this._timer) {
+    action();
+    this._timer = window.setTimeout(
+      function() {
+        that._timer = null;
+      },
+      duration
+    );
+  }
+  return this;
+};
+
+
+
+/**
+ * @param action A function returning a Promise.
+ */
 exports.laterPromise = function(action, delay) {
+    return new ActionPromise(
+        function() {
+            return new Promise(action);
+        },
+        delay
+    );
+};
+
+/**
+ * @param action A function to execute.
+ */
+exports.laterAction = function(action, delay) {
     return new Action(action, delay);
 };
+
+
+exports.longAction = function() {
+  return new LongAction();
+}
