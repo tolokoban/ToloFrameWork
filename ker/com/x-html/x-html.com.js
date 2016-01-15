7bb1f1640666c1fd1e4a1d3de9da28387b73cf3b
Tolokoban <contact@tolokoban.org>
2016-01-15 17:57:35 +0100

    Adaptation to the old classes.

--- ker/com/x-html/x-html.com.js
+++ ker/com/x-html/x-html.com.js
@@ -6,23 +6,6 @@ exports.tags = ["x-html", "w:html"];
 exports.priority = 0;
 
 /**
- * Called the  first time the  component is  used in the  complete build
- * process.
- */
-exports.initialize = function(libs) {};
-
-/**
- * Called the first time the component is used in a specific HTML file.
- */
-exports.open = function(file, libs) {};
-
-/**
- * Called after a specific HTML file  as been processed. And called only
- * if the component has been used in this HTML file.
- */
-exports.close = function(file, libs) {};
-
-/**
  * Compile a node of the HTML tree.
  */
 exports.compile = function(root, libs) {
