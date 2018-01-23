868dd4d0229f479e5ce4884df400859690e1fc1f
Fabien Petitjean <f.petitjean@lombardodier.com>
2018-01-23 18:16:45 +0100

    We want a new option that specify the path for karma specs.

--- bin/tfw.js
+++ bin/tfw.js
@@ -1,6 +1,46 @@
 #! /usr/bin/env node
 // -*- js -*-
 
+var DEF = {
+  init: {
+    desc: "start a fresh new project."
+  },
+  clean: {
+    desc: "remove all temporary files."
+  },
+  build: {
+    desc: "compile project in the www/ folder."
+  },
+  debug: {
+    desc: "JS and CSS files won't be minified."
+  },
+  php: {
+    desc: "add PHP services."
+  },
+  test: {
+    desc: "prepare Karma tests.",
+    opts: {
+      dir: {
+        desc: "Karma spec folder. Default is '-dir spec'.",
+        args: ["spec"]
+      }
+    }
+  },
+  doc: {
+    desc: "create documentation."
+  },
+  jsdoc: {
+    desc: "create JSDoc documentation."
+  },
+  watch: {
+    desc: "watch for files change."
+  },
+  version: {
+    desc: "increment version number."
+  }
+};
+
+
 /**
  *
  * @module tfw
