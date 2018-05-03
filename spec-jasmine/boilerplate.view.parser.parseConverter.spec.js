87faf6b7171c3ea13a4d438d47a1307ad29c6c2e
Tolokoban <fabien.petitjean@free.fr>
2018-05-03 21:43:10 +0200

    ...

---
+++ spec-jasmine/boilerplate.view.parser.parseConverter.spec.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var Parser = require("../lib/boilerplate.view.parser.constructor");
+require("../lib/boilerplate.view.parser.names")(Parser);
+require("../lib/boilerplate.view.parser.parseConverter")(Parser);
+
+describe('Module `boilerplate.view.parser.parseConverter`', function() {
+  
+});
+
+
