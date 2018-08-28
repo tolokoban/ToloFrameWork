331a27ad5a4bd238d8877cce9f430d4af4b68a71
Fabien Petitjean <f.petitjean@lombardodier.com>
2018-08-28 11:49:12 +0200

    Combo now works on Firefox.

--- ker/mod/tfw.view.combo.js
+++ ker/mod/tfw.view.combo.js
@@ -1,65 +1,129 @@
 "use strict";
 
 var CODE_BEHIND = {
+  onIndexChange: onIndexChange,
+  onKeysChange: onKeysChange,
   onItemsChange: onItemsChange,
   onValueChange: onValueChange,
   onExpandedChange: onExpandedChange
 };
 
 var $ = require("dom");
+var PM = require("tfw.binding.property-manager");
+var Touchable = require("tfw.touchable");
+
 
 function onItemsChange( items ) {
   var that = this;
   var list = $(this.$elements.list);
   var button = $(this.$elements.button);
   $.clear( list );
-  this._itemsDivs = [];
   this._listHeight = 0;
+  this._itemsDivs = [];
 
-  Object.keys(items).forEach(function (key, index) {
+  items.forEach(function (val, index) {
     that._listHeight += 32;
-    var val = items[key];
     var item = $.div([ val ]);
-    that._itemsDivs.push([ key, item ]);
+    that._itemsDivs.push( item );
     $.add( list, item );
-    $.on( item, function() {
-      if( that.expanded ) {
-        $.css( list, {
-          transform: "translateY(-" + 32*index + "px)",
-          left: 0, top: 0,
-          height: "auto"
-        });
-      } else {
-        $.addClass( item, 'thm-bgSL' );
-        var rect = button.getBoundingClientRect();
-        var top = rect.top - 32 * index;
-        var topLimit = top - 32 * Math.floor( top / 32 );
-        if( top < 0 ) top = topLimit;
-        while( top > topLimit && top + that._listHeight > document.body.clientHeight - 8 ) {
-          top -= 32;
-        }
-        top += 32 * index;
-        $.css( list, {
-          left: rect.left + "px",
-          top: top + "px",
-          height: "auto"
-        });
-      }
-      that.expanded = !that.expanded;
-    });
+    var touchable = new Touchable( item );
+    touchable.tap.add( onItemTap.bind( that, item, list, index, button ) );
   });
 }
 
 
-function onValueChange( value ) {
+function onItemTap( item, list, index, button ) {
+  console.info("[tfw.view.combo] this.expanded=", this.expanded);
+  if( this.expanded ) {
+    // List is expanded.
+    $.css( list, {
+      transform: "translateY(-" + 32*index + "px)",
+      left: 0, top: 0,
+      height: "auto"
+    });
+  } else {
+    // List is collapsed.
+    $.addClass( item, 'thm-bgSL' );
+    if( getLength.call( this, this.items ) === 2 ) {
+      this.index = 1 - this.index;
+      return;
+    }
+    var rect = button.getBoundingClientRect();
+    var top = rect.top - 32 * index;
+    var topLimit = top - 32 * Math.floor( top / 32 );
+    if( top < 0 ) top = topLimit;
+    while( top > topLimit && top + this._listHeight > document.body.clientHeight - 8 ) {
+      top -= 32;
+    }
+    top += 32 * index;
+    $.css( list, {
+      left: rect.left + "px",
+      top: top + "px",
+      height: "auto"
+    });
+    $.css( button, {
+      width: this.wide ? 'auto' : list.getBoundingClientRect().width + 'px'
+    });
+  }
+  this.index = index;
+  this.expanded = !this.expanded;
+}
 
+
+function onIndexChange( index ) {
+  if( hasKeys.call( this ) ) {
+    this.value = this.keys[index];
+  }
+  else {
+    this.value = index;
+  }
+}
+
+
+function hasKeys() {
+  return getLength.call( this, this.keys ) === getLength.call( this, this.items );
+}
+
+
+function getLength( arr ) {
+  if( !arr ) return 0;
+  if( !Array.isArray( arr ) ) return 0;
+  return arr.length;
+}
+
+
+function onKeysChange( keys ) {
+  if( typeof this.index !== 'number' ) return;
+  onIndexChange.call( this, this.index );
+}
+
+
+function onValueChange( value ) {
+  var index;
+  if( hasKeys.call( this ) ) {
+    index = this.keys.indexOf( value );
+    if( index < 0 ) index = 0;
+    PM( this ).set( "index", index );
+  }
+  else {
+    index = parseInt( value );
+    if( !isNaN( index ) && index >= 0 && index < getLength.call( this, this.items ) ) {
+      PM( this ).set( "index", index );
+    }
+  }
+  var list = $(this.$elements.list);
+  $.css( list, {
+    transform: "translateY(-" + 32*this.index + "px)",
+    left: 0, top: 0,
+    height: "auto"
+  });
 }
 
 
 function onExpandedChange( expanded ) {
   if( !expanded ) {
     this._itemsDivs.forEach(function (itemDiv) {
-      $.removeClass( itemDiv[1], 'thm-bgSL' );
+      $.removeClass( itemDiv, 'thm-bgSL' );
     });
   }
 }
