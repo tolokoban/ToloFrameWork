4c4159f571a5e53d1b4a87a072ae2e44a7cf43bb
Fabien Petitjean <f.petitjean@lombardodier.com>
2018-12-13 16:50:20 +0100

    ...

--- ker/mod/tfw.color.js
+++ ker/mod/tfw.color.js
@@ -100,6 +100,7 @@ const
 
 /**
  * Read R,G,B and write H,S,L.
+ * @this Color
  * @returns {undefined}
  */
 function rgb2hsl() {
@@ -133,8 +134,9 @@ function rgb2hsl() {
 }
 
 /**
- * @returns {undefined}
  * @see https://en.wikipedia.org/wiki/HSL_and_HSV#Converting_to_RGB
+ * @this Color
+ * @returns {undefined}
  */
 function hsl2rgb() {
     const
@@ -185,14 +187,16 @@ function hsl2rgb() {
 }
 
 /**
- * @returns {undefined}
  * @see https://en.wikipedia.org/wiki/Grayscale
+ * @this Color
+ * @returns {undefined}
  */
 function luminance() {
     return ( 0.2126 * this.R ) + ( 0.7152 * this.G ) + ( 0.0722 * this.B );
 }
 
 /**
+ * @this Color
  * @returns {string} String value of the color. `#fd45a7`.
  */
 function stringify() {
@@ -205,6 +209,7 @@ function stringify() {
 
 
 /**
+ * @this Color
  * @param {string} _text - CSS color code.
  * @returns {boolean} Success?
  */
@@ -222,6 +227,7 @@ function parse( _text ) {
 
 
 /**
+ * @this Color
  * @param {string} text - CSS color code.
  * @returns {boolean} Success?
  */
@@ -234,29 +240,29 @@ function parseHexa( text ) {
         A = 1;
 
     switch ( text.length ) {
-        case 4:
-            R = parseInt( text.charAt( 1 ), 16 ) * INV15;
-            G = parseInt( text.charAt( 2 ), 16 ) * INV15;
-            B = parseInt( text.charAt( 3 ), 16 ) * INV15;
-            break;
-        case 5:
-            R = parseInt( text.charAt( 1 ), 16 ) * INV15;
-            G = parseInt( text.charAt( 2 ), 16 ) * INV15;
-            B = parseInt( text.charAt( 3 ), 16 ) * INV15;
-            A = parseInt( text.charAt( 4 ), 16 ) * INV15;
-            break;
-        case 7:
-            R = parseInt( text.substr( 1, 2 ), 16 ) * INV255;
-            G = parseInt( text.substr( 3, 2 ), 16 ) * INV255;
-            B = parseInt( text.substr( 5, 2 ), 16 ) * INV255;
-            break;
-        case 9:
-            R = parseInt( text.substr( 1, 2 ), 16 ) * INV255;
-            G = parseInt( text.substr( 3, 2 ), 16 ) * INV255;
-            B = parseInt( text.substr( 5, 2 ), 16 ) * INV255;
-            A = parseInt( text.substr( 7, 2 ), 16 ) * INV255;
-            break;
-        default:
+    case 4:
+        R = parseInt( text.charAt( 1 ), 16 ) * INV15;
+        G = parseInt( text.charAt( 2 ), 16 ) * INV15;
+        B = parseInt( text.charAt( 3 ), 16 ) * INV15;
+        break;
+    case 5:
+        R = parseInt( text.charAt( 1 ), 16 ) * INV15;
+        G = parseInt( text.charAt( 2 ), 16 ) * INV15;
+        B = parseInt( text.charAt( 3 ), 16 ) * INV15;
+        A = parseInt( text.charAt( 4 ), 16 ) * INV15;
+        break;
+    case 7:
+        R = parseInt( text.substr( 1, 2 ), 16 ) * INV255;
+        G = parseInt( text.substr( 3, 2 ), 16 ) * INV255;
+        B = parseInt( text.substr( 5, 2 ), 16 ) * INV255;
+        break;
+    case 9:
+        R = parseInt( text.substr( 1, 2 ), 16 ) * INV255;
+        G = parseInt( text.substr( 3, 2 ), 16 ) * INV255;
+        B = parseInt( text.substr( 5, 2 ), 16 ) * INV255;
+        A = parseInt( text.substr( 7, 2 ), 16 ) * INV255;
+        break;
+    default:
     }
 
     if ( isNaN( R ) || isNaN( G ) || isNaN( B ) || isNaN( A ) ) {
@@ -273,36 +279,51 @@ function parseHexa( text ) {
 
 const RX_RGB = /^RGB[\s\(]+([0-9]+)[^0-9]+([0-9]+)[^0-9]+([0-9]+)/;
 
+/**
+ * @this Color
+ * @param   {string} text - `rgb(200, 140, 50)`
+ * @returns {boolean} `true` if `text` is a valid `rgb()` syntax.
+ */
 function parseRGB( text ) {
     const m = RX_RGB.exec( text );
     if ( !m ) return false;
-    this.R = clamp01( parseInt( m[ 1 ] ) * INV255 );
-    this.G = clamp01( parseInt( m[ 2 ] ) * INV255 );
-    this.B = clamp01( parseInt( m[ 3 ] ) * INV255 );
+    this.R = clamp01( parseInt( m[ 1 ], 10 ) * INV255 );
+    this.G = clamp01( parseInt( m[ 2 ], 10 ) * INV255 );
+    this.B = clamp01( parseInt( m[ 3 ], 10 ) * INV255 );
     this.A = 1;
     return true;
 }
 
-var RX_RGBA = /^RGBA[\s\(]+([0-9]+)[^0-9]+([0-9]+)[^0-9]+([0-9]+)[^0-9\.]+([0-9\.]+)/;
+const RX_RGBA = /^RGBA[\s\(]+([0-9]+)[^0-9]+([0-9]+)[^0-9]+([0-9]+)[^0-9\.]+([0-9\.]+)/;
 
+/**
+ * @this Color
+ * @param   {string} text - `rgba(200, 140, 50, 0.5)`
+ * @returns {boolean} `true` if `text` is a valid `rgba()` syntax.
+ */
 function parseRGBA( text ) {
     const m = RX_RGBA.exec( text );
     if ( !m ) return false;
-    this.R = clamp01( parseInt( m[ 1 ] ) * INV255 );
-    this.G = clamp01( parseInt( m[ 2 ] ) * INV255 );
-    this.B = clamp01( parseInt( m[ 3 ] ) * INV255 );
+    this.R = clamp01( parseInt( m[ 1 ], 10 ) * INV255 );
+    this.G = clamp01( parseInt( m[ 2 ], 10 ) * INV255 );
+    this.B = clamp01( parseInt( m[ 3 ], 10 ) * INV255 );
     this.A = clamp01( parseFloat( m[ 4 ] ) );
     return true;
 }
 
 const RX_HSL = /^HSL[\s\(]+([0-9]+)[^0-9]+([0-9]+)[^0-9]+([0-9]+)/;
 
+/**
+ * @this Color
+ * @param   {string} text - `hsl(200, 140, 50)`
+ * @returns {boolean} `true` if `text` is a valid `hsl()` syntax.
+ */
 function parseHSL( text ) {
     const m = RX_HSL.exec( text );
     if ( !m ) return false;
-    this.H = clamp01( parseInt( m[ 1 ] ) * INV359 );
-    this.S = clamp01( parseInt( m[ 2 ] ) * INV99 );
-    this.L = clamp01( parseInt( m[ 3 ] ) * INV99 );
+    this.H = clamp01( parseInt( m[ 1 ], 10 ) * INV359 );
+    this.S = clamp01( parseInt( m[ 2 ], 10 ) * INV99 );
+    this.L = clamp01( parseInt( m[ 3 ], 10 ) * INV99 );
     this.A = 1;
     this.hsl2rgb();
     return true;
@@ -362,4 +383,3 @@ function staticLuminance( text ) {
     const color = staticParse( text );
     return color.luminance();
 }
-
